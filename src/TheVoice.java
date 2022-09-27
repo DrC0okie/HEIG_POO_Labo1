2d7513f7a39a0aa0b72ad9810befac8d43d26708
DrC0okie <timothee.vanhove@heig-vd.ch>
2022-09-27 21:39:42 +0200

    Finished

--- src/TheVoice.java
+++ src/TheVoice.java
@@ -1,30 +1,31 @@
 public class TheVoice {
+    static final int NB_VOTES = 150;
+
     public static void main(String[] args) {
-        int NB_VOTES = 150;
         int nameCount = args.length;
         if (nameCount == 0)
             System.out.println("Il n'existe pas de candidats");
-        else{
-            int value;
+        else {
+            System.out.println("Candidats:");
+            for (int i = 0; i < nameCount; ++i)
+                System.out.println("#" + (i + 1) + " " + args[i]);
+
+            java.util.Random random = new java.util.Random();
             int[] count = new int[nameCount];
-            for (int i = 0; i < NB_VOTES; i++){
-                java.util.Random random = new java.util.Random();
-                value = random.nextInt(nameCount);
-                count[value] ++;
-            }
-            int i = 1;
-            for (String name : args) {
-                System.out.println("#" + i + " " + name);
-                name += (count[i-1] * 100 / NB_VOTES);
-                i++;
+
+            System.out.println("\n" + NB_VOTES + " votes:");
+
+            for (int i = 0; i < NB_VOTES; ++i) {
+                count[random.nextInt(nameCount)]++;
+                System.out.print('.');
             }
 
-            System.out.println("Résultats");
-            for (String name : args) {
-                System.out.println(name);
+            System.out.println("\n\nResultats : ");
+
+            for (int i = 0; i < nameCount; ++i) {
+                int percent = count[i] * 100 / NB_VOTES;
+                System.out.println(percent + "% " + args[i]);
             }
         }
-
-
     }
 }
